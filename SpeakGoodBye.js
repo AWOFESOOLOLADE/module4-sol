c9b8fbb05f08b9a4afb26ed5e314ea29c48ad24b
AWOFESOOLOLADE <140724244+AWOFESOOLOLADE@users.noreply.github.com>
2023-09-19 16:17:16 +0100

    Add files via upload

---
+++ SpeakGoodBye.js
@@ -0,0 +1,8 @@
+(function (window) {
+var GoodBye = {};
+var byeSpeaker =  "Good Bye ";
+GoodBye.SayGoodbye = function () {
+  console.log(byeSpeaker + names[i]);
+}
+ window.GoodBye = GoodBye;
+})(window);
---
+++ SpeakHello.js
@@ -0,0 +1,8 @@
+(function (window) {
+var Hello = {};
+var helloSpeaker = "Hello ";
+Hello.SayHello = function () {
+  console.log(helloSpeaker + names[i]);
+}
+  window.Hello = Hello;
+})(window);
